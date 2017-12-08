d56b676e3379e41f5fcb61b51daf6a1497782d74
CarlosTheG <Carlos@Carloss-MacBook-Pro.local>
2017-12-07 19:38:06 -0600

    built up pong.py file

--- pong/pong.py
+++ pong/pong.py
@@ -1,3 +1,41 @@
 '''
 Runs a Pong Q Learned network
 '''
+
+import gym
+import matplotlib.pyplot as plt
+import time
+
+env = gym.make('Pong-v0')
+state = env.reset()
+
+# 6 actions
+print (env.action_space)
+# 210x160x3 input image
+print (env.observation_space)
+
+
+
+for i in range(20):
+    #plt.imshow(s)
+    # take a random action. Returns state which is new game state
+    state, reward, done, _ = env.step(env.action_space.sample())
+    env.render()
+    # plt.imshow(state)
+    # plt.show()
+
+env.close()
+
+
+
+'''
+0-1 nothing, 2 upward, 3 downward, 4 upward, 5 downward
+** From this we should simplify our output to 3 actions [0, 2, 3] **
+'''
+def test_actions():
+    for i in range(2, 4, 1):
+        print ("testing action: ", i)
+        for j in range(10):
+            env.step(i)
+            env.render()
+            time.sleep(0.2)
