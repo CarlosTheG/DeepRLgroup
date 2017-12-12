1d5a7c154640383dcf3b83c79960219009621ed3
CarlosTheG <Carlos@Carloss-MacBook-Pro.local>
2017-12-12 14:45:52 -0600

    chilling

--- lunarlander/restore_model.py
+++ lunarlander/restore_model.py
@@ -2,25 +2,44 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
 from keras import optimizers
+import numpy as np
 import gym
 import os
 
 
-weigths_filename = "LL-QL-v2-weights2.h5"
+weigths_filename = "weights-QSAL.h5"
 
 env = gym.make('LunarLander-v2')
 
 model = Sequential()
 #model.add(Dense(num_env_variables+num_env_actions, activation='tanh', input_dim=dataX.shape[1]))
-model.add(Dense(512, activation='relu', input_dim=8))
+model.add(Dense(512, activation='relu', input_dim=12))
 model.add(Dense(250, activation='relu', input_dim=512))
-model.add(Dense(4))
-
+model.add(Dense(1))
 opt = optimizers.adam(lr=0.001)
 model.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
 
+#One hot encoding array
+possible_actions = np.arange(0,4)
+actions_1_hot = np.zeros((4,4))
+actions_1_hot[np.arange(4),possible_actions] = 1
+
+
+def get_policy(s):
+    preds = np.empty(shape=[4], dtype=np.float32)
+    for action in range(4):
+        s_a = np.concatenate((s,actions_1_hot[action]), axis=0)
+        predX = np.zeros(shape=(1,12))
+        predX[0] = s_a
+
+        #print("trying to predict reward at qs_a", predX[0])
+        ex = predX[0].reshape(1,predX.shape[1])
+        pred = model.predict(predX[0].reshape(1,predX.shape[1]))
+        preds[action] = pred[0][0]
+    return np.argmax(preds)
+
 #load previous model weights
-if load_previous_weights:
+if True:
     dir_path = os.path.realpath(".")
     fn = dir_path + "/"+weigths_filename
     print("filepath ", fn)
@@ -35,12 +54,10 @@ reward = 0
 for i in range(50):
     s = env.reset()
     while True:
-        a = model.predict(np.array([s]))[0]
-        action = np.argmax(a)
+        action = get_policy(s)
         s,r,d,_ = env.step(action) #observation, reward, done, info
         reward += r
         if d == True:
             break
         env.render()
-        time.sleep(0.001)
 print(reward/50.0)
